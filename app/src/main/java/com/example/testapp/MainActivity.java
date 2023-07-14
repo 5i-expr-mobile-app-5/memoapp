ba9f035bb94e2d832a404d2ca03dd08d8addfec6
fla9 <72453062+fla9@users.noreply.github.com>
2023-07-14 13:34:46 +0900

    入力・送信部分
    入力フォーム・送信ボタンの実装をしました
    入力した文章は画面中央に表示されます

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.testapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+
+        textView = findViewById(R.id.text_view);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener( v-> {
+            String text = editText.getText().toString();
+            textView.setText(text);
+            editText.getEditableText().clear();
+        });
     }
 }
