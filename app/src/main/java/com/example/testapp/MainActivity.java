144b791122a6846fe708da1f39445b73cbf6c3b8
st19178ik <82067496+st19178ik@users.noreply.github.com>
2023-07-14 17:03:09 +0900

    リストのアイテムを追加

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.testapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] FROM = {"date","time","content"};
+    private static final int[] TO = {R.id.date,R.id.time,R.id.content};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.note_view);
     }
 }
